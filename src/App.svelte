1c0619c42370182c5008bb09a6385b0f6f6d26c1
toc <tomvarewyck98@gmail.com>
2023-07-11 21:51:59 +0200

    Using parallax from sveltetronics and glassmorphism

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { parallax } from "sveltronics";
+</script>
+
+<main>
+  <div class="card" use:parallax={[700, 10]}>
+    <div class="card__content">
+      VareToc
+    </div>
+  </div>
+</main>
+
+<style lang="scss">
+  main {
+    display: grid;
+    align-items: center;
+  }
+  .card {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 10rem;
+    width: 20rem;
+    background: rgba( 255, 255, 255, 0.25 );
+    box-shadow: 0 .8rem 3.2rem 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( .4rem );
+    -webkit-backdrop-filter: blur( .4rem );
+    border-radius: 1rem;
+    border: .1rem solid rgba( 255, 255, 255, 0.18 );
+
+    &__content {
+      font-size: 2.4rem;
+    }
+  }
+</style>
